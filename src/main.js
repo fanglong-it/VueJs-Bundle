842b91144b995a836e04203b1be22d5f0d91ac4b
fanglong-it <fang.longpc@gmail.com>
2023-04-19 15:19:54 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue';
+import { useToast } from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-sugar.css';
+const $toast = useToast();
+const app = createApp({
+    methods: {
+        open(){
+            console.log("Open function!");
+            $toast.open({
+                message: "Test message from Vue",
+                type: "success",
+                duration: 3000,
+                dismissible: true
+            });
+        }
+    }
+
+});
+app.mount("#app");
---
+++ src/select-2/select2.js
@@ -0,0 +1,38 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        selectedValues: []
+      }
+    },
+    mounted() {
+      $(".js-example-placeholder-multiple").select2({
+        placeholder: "Select a user",
+        ajax: {
+          url: "https://jsonplaceholder.typicode.com/users",
+          dataType: 'json',
+          delay: 250,
+          processResults: function (data) {
+            return {
+              results: data.map(function (user) {
+                return {
+                  id: user.id,
+                  text: user.name
+                };
+              })
+            };
+          },
+          cache: true
+        }
+      }).on('change', () => {
+        this.selectedValues = $('.js-example-placeholder-multiple').val();
+      });
+    }
+  });
+
+  app.mount('#app');
+  
+  
+  
+  
+  
+  
